be8cbcc1fb224b01f17e12b229be747e1f0daaf7
setgodude <101807359+Nam049499@users.noreply.github.com>
2023-08-19 10:30:13 +0700

    Update loopmusic.js

--- loopmusic.js
+++ loopmusic.js
@@ -1,5 +1,5 @@
 var sound = new Howl({
-  src: ['y2mate.com - Russian Alphabet Lore Reloaded OST  Explosion of Happiness.webm', 'y2mate.com - Russian Alphabet Lore Reloaded OST  Explosion of Happiness.mp3', 'y2mate.com - Russian Alphabet Lore Reloaded OST  Explosion of Happiness.wav'],
+  src: ['y2mate.com - Russian Alphabet Lore Reloaded OST  Explosion of Happiness.mp3'],
   autoplay: true,
   loop: true,
   volume: 0.5,
